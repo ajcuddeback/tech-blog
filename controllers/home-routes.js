277c7ba6299785a089c7d89a71c4d6d28f754b1e
ajcudd <ajcuddeback@gmail.com>
2021-01-31 18:12:25 -0500

    added front end get route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection')
+const { User, Post, Comment } = require('../models');
 
-const { User, Post, Comment } = require('../models')
+router.get('/', (req, res) => {
+    Post.findAll({
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
