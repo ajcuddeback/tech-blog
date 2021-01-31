98bdeb71d71c58a6610e0641331dc28a3b89390c
ajcudd <ajcuddeback@gmail.com>
2021-01-31 17:40:53 -0500

    added file structure for front end

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-const routes = require('./controllers')
+const routes = require('./controllers');
+
 
 const app = express();
 
