5fe4b5caaecb845cff5e100dfc578412bb730345
ajcudd <ajcuddeback@gmail.com>
2021-01-31 09:01:01 -0500

    added post route for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,3 +45,18 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err)
         })
 })
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+module.exports = router;
