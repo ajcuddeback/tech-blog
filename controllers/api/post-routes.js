1c07180b65d9876657bef8b6abb6b20061a92890
ajcudd <ajcuddeback@gmail.com>
2021-01-31 09:41:00 -0500

    added get route for one post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,3 +25,36 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         })
 });
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found at this id' })
+            }
+
+            res.json(dbPostData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
