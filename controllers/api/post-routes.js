a5d26d6402580b845126c9f6a2a32bd6926e9665
ajcudd <ajcuddeback@gmail.com>
2021-01-31 09:56:54 -0500

    added put route for post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -72,4 +72,26 @@ router.post('/', (req, res) => {
         })
 });
 
+router.put('/:id', (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found at this id' });
+                return;
+            }
+
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+
+
 module.exports = router;
