7cb44d3d5b58f6251e2a756e3bba3fe0ff430e68
ajcudd <ajcuddeback@gmail.com>
2021-02-01 10:21:20 -0500

    added ability to login, signup, and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
             req.session.save(() => {
                 req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
-                res.session.loggedIn = true;
+                req.session.loggedIn = true;
 
                 res.json(dbUserData);
             });
@@ -86,7 +86,7 @@ router.post('/login', (req, res) => {
             }
 
             req.session.save(() => {
-                res.session.user_id = dbUserData.id;
+                req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
                 req.session.loggedIn = true;
 
